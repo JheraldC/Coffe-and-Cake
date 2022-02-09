6b415d469a98f0d327f85994632f1c8b059146a7
Jherald <jcaceresap@unsa.edu.pe>
2022-02-09 06:35:34 -0500

    creando los metodos para que el slide funcione

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,26 @@
-var propSlider = {
-  slider: document.getElementById('slider'),
-  primerSlider: null,
-}
-
-var metSlider = {
-  inicio: function(){
-    setInterval(metSlider.moverSlide, 3000);
-  },
-  moverSlide: function(){
-    propSlider.slider.style.transition = 'all 1s ease';
+(function(){
+  var propSlider = {
+    slider: document.getElementById('slider'),
+    primerSlider: null,
   }
-}
 
-metSlider.inicio();
+  var metSlider = {
+    inicio: function(){
+      setInterval(metSlider.moverSlide, 3000);
+    },
+    moverSlide: function(){
+      propSlider.slider.style.transition = 'all 1s ease';
+      propSlider.slider.style.marginLeft = '-100%';
+
+      setTimeout(function(){
+        propSlider.primerSlider = propSlider.slider.firstElementChild;
+        propSlider.slider.appendChild(propSlider.primerSlider);
+        propSlider.slider.style.transition = 'unset';
+        propSlider.slider.style.marginLeft = '0%';
+      }, 1000);
+    }
+  }
+
+  metSlider.inicio();
+
+}())
