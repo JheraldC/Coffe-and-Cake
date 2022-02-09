27ba07bcc03e10bf132ca5fb7c9e20348bc06eb6
Jherald <jcaceresap@unsa.edu.pe>
2022-02-09 04:50:27 -0500

    Dandole estilo al lightbox container y mostrando la imagen

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,7 +3,8 @@ var propLightbox = {
   image: null,
   imageSrc: null,
   cuerpoDom: document.getElementsByTagName('body')[0],
-  lightbox_container: null;
+  lightbox_container: null,
+  modal: null
 }
 
 var metLightbox = {
@@ -20,6 +21,20 @@ var metLightbox = {
     propLightbox.imageSrc = window.getComputedStyle(imagen, null).backgroundImage.slice(5,-2);
     propLightbox.cuerpoDom.appendChild(document.createElement('div')).setAttribute('id', 'lightbox_container');
     propLightbox.lightbox_container = document.getElementById('lightbox_container');
+
+    propLightbox.lightbox_container.style.width = '100%';
+    propLightbox.lightbox_container.style.height = '100%';
+    propLightbox.lightbox_container.style.position = 'fixed';
+    propLightbox.lightbox_container.style.zIndex = '1000';
+    propLightbox.lightbox_container.style.background = 'rgba(0,0,0,0.8)';
+    propLightbox.lightbox_container.style.top = '0';
+    propLightbox.lightbox_container.style.left = '0';
+
+    propLightbox.lightbox_container.appendChild(document.createElement('div')).setAttribute('id', 'modal');
+    propLightbox.modal = document.getElementById('modal');
+    propLightbox.modal.style.height = '100%';
+    propLightbox.modal.appendChild(document.createElement('img')).setAttribute('src', propLightbox.imageSrc);
+    propLightbox.modal.getElementsByTagName('img')[0].setAttribute('class', 'img-modal');
   }
 }
 
