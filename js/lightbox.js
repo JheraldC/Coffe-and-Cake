ac17494fc86ca938cb91a4f0f0fc2caea0d1173c
Jherald <jcaceresap@unsa.edu.pe>
2022-02-09 04:24:32 -0500

    dandole la funcion para recuperar el background image

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,19 +1,21 @@
 var propLightbox = {
   imgContainer: document.getElementsByClassName('lightbox'),
-  image: null;
+  image: null,
+  imagensrc: null
 }
 
 var metLightbox = {
-  inicio: function(){
+  inicio: function() {
     for (var i = 0; i < propLightbox.imgContainer.length; i++) {
       propLightbox.imgContainer[i].addEventListener('click', metLightbox.capturaImagen)
     }
   },
-  capturaImagen: function(){
+  capturaImagen: function() {
     propLightbox.image = this;
     metLightbox.lightbox(propLightbox.image);
   },
-  lightbox: function(imagen){
-    
+  lightbox: function(imagen) {
+    propLightbox.imagensrc = window.getComputedStyle(imagen, null).backgroundImage;
+    console.log(propLightbox.imagensrc);
   }
 }
