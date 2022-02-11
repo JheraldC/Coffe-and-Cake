ec05179d39711bad6d7b370f3736c0304a990329
Jherald <jcaceresap@unsa.edu.pe>
2022-02-11 18:34:24 -0500

    Añadiendo las funciones a metParallax y añadiendo valores a propParallax

--- js/bgParallax.js
+++ js/bgParallax.js
@@ -1,10 +1,22 @@
 
 var propParallx = {
 
-  seccion: document.querySelector('.parallax');
+  seccion: document.querySelector('.parallax'),
+  recorrido: null,
+  limite: null,
 
 }
 
 var metParallx = {
 
+  inicio: function(){
+    window.addEventListener('scroll', metParallx.scrollParallax);
+  },
+
+  scrollParallax: function(){
+
+  }
+
 }
+
+metParallx.inicio();
