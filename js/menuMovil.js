845035f74e74781dcd5313c32a28d2b2fc9f82c9
Jherald <jcaceresap@unsa.edu.pe>
2022-02-12 13:55:25 -0500

    añadiendo las propiedades del menu movil

---
+++ js/menuMovil.js
@@ -0,0 +1,11 @@
+
+var propMenu = {
+  burger_menu: document.getElementById('burger_menu'),
+  slideMenu: document.getElementById('slideMenu'),
+  menu_activo: false,
+  elem_menu: 
+}
+
+var metMenu = {
+
+}
