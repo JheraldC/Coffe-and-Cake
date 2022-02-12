f3cbd4dd9cd6529eda428f674f3c2e4bbb7d71db
Jherald <jcaceresap@unsa.edu.pe>
2022-02-11 22:02:54 -0500

    creo las propiedades y metodos, entre ellos el addevent listener

--- js/formulario.js
+++ js/formulario.js
@@ -1,8 +1,25 @@
 
 var propFormulario = {
+  formulario: document.getElementsByName('formulario_contacto')[0],
+  elementos: document.getElementsByName('formulario_contacto')[0].elements,
 
 }
 
 var metFormulario = {
-  
+  Inicio: function(){
+    for (var i = 0; i < propFormulario.elementos.length; i++) {
+      if (propFormulario.elementos[i].type != 'submit') {
+        propFormulario.elementos[i].addEventListener('focus', metFormulario.focusInput);
+        propFormulario.elementos[i].addEventListener('blur', metFormulario.blurInput);
+      }
+    }
+  },
+  focusInput: function(){
+    
+  },
+  blurInput: function(){
+
+  }
 }
+
+metFormulario.Inicio();
