44ddeee8655516cbdca0cc9306517160e41f3145
Jherald <jcaceresap@unsa.edu.pe>
2022-02-11 23:27:33 -0500

    Creando propiedades, ademas de los metodos moverse

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,14 @@
 
 var propScroll = {
-
   posicion: window.pageYOffset,
   scroll_suave: document.getElementsByClassName('scroll-suave'),
   volver_arriba: document.getElementsByClassName('volver-arriba'),
+  destino: null,
+  seccion_distancia: null
 }
 
 var metScroll = {
-
-  inicio: function(){
+  Inicio: function(){
     for (var i = 0; i < propScroll.scroll_suave.length; i++) {
       propScroll.scroll_suave[i].addEventListener('click', metScroll.moverse);
     }
@@ -16,11 +16,15 @@ var metScroll = {
       propScroll.volver_arriba[i].addEventListener('click', metScroll.subir);
     }
   },
-  moverse: function(){
-
+  moverse: function(e){
+    e.preventDefault();
+    propScroll.destino = this.getAttribute('href');
+    propScroll.seccion_distancia = document.querySelector(propScroll.destino);
+    console.log(propScroll.seccion_distancia);
   },
   subir: function(){
-    
-  }
 
+  }
 }
+
+metScroll.Inicio();
