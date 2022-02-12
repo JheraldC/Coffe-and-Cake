4f7ba78a058d6969ed88be967b4352c22b99e7cf
Jherald <jcaceresap@unsa.edu.pe>
2022-02-12 13:59:04 -0500

    Añadiendo los elementos del menu

--- js/menuMovil.js
+++ js/menuMovil.js
@@ -3,7 +3,8 @@ var propMenu = {
   burger_menu: document.getElementById('burger_menu'),
   slideMenu: document.getElementById('slideMenu'),
   menu_activo: false,
-  elem_menu: 
+  elem_menu: document.querySelectorAll('#slideMenu .menu-principal a'),
+  
 }
 
 var metMenu = {
