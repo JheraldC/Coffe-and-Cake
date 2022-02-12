fffb49d7a635eb1307b86e656339e5cf58dc982e
Jherald <jcaceresap@unsa.edu.pe>
2022-02-11 22:49:29 -0500

    Modificando la condicional por error, ademas creando el caso contrario

--- js/formulario.js
+++ js/formulario.js
@@ -27,12 +27,18 @@ var metFormulario = {
     for (var i = 0; i < propFormulario.elementos.length - 1; i++) {
       if (propFormulario.elementos[i].value == '') {
         event.preventDefault();
-        if (propFormulario.elementos[i].children.length < 3) {
+        if (propFormulario.elementos[i].parentElement.childElementCount < 3) {
           propFormulario.error = document.createElement('p');
           propFormulario.textoError = document.createTextNode('Por favor, llena el campo con tu ' + propFormulario.elementos[i].name);
           propFormulario.error.appendChild(propFormulario.textoError);
           propFormulario.error.className = 'error';
           propFormulario.elementos[i].parentElement.appendChild(propFormulario.error);
+        }
+      }
+      else{
+        if ((propFormulario.elementos[i].parentElement.childElementCount >= 3) {
+          propFormulario.error = propFormulario.elementos[i].parentElement.getElementsByTagName('p')[0];
+          
         }
       }
     }
